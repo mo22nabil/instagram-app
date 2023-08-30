4e7a5acac0f7df94e9fd248462cdef0f3aeadfac
mohamed Nabil <61280396+mo22nabil@users.noreply.github.com>
2023-08-31 01:03:08 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 const connectDB = ()=>{
-    return mongoose.connect(process.env.DBURL)
+    return mongoose.connect(process.env.DBURL2)
     .then(result=>console.log(`connect to DB on URL   .... ${process.env.DBURL}`))
     .catch(err=>console.log(`fail to connect DB`))
 }
